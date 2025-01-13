9fbdbdc679c67a78073b4db859d52a4f4120f9e0
Israel Fernandez <israelfernandez96@gmail.com>
2025-01-13 14:20:53 -0800

    Added another method of summation

--- JavaScriptFireship/sum.js
+++ JavaScriptFireship/sum.js
@@ -1,12 +1,12 @@
-// function summation(nums) {
-//     let sum = 0;
-//     for (let num of nums) {
-//         sum += num;
-//     }
-//     return sum;
-// }
+function summation(nums) {
+    let sum = 0;
+    for (let num of nums) {
+        sum += num;
+    }
+    return sum;
+}
 
 let numbersToSum = [4, 1, 5, 123, 45, 1, 34, 23];
-// let numbers = summation(numbersToSum);
-const numbers = numbersToSum.reduce((accumulator, current) => accumulator + current, 0);
+let numbers = summation(numbersToSum);
+// const numbers = numbersToSum.reduce((accumulator, current) => accumulator + current, 0);
 console.log(numbers);
